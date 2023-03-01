2c1c5df7fce1918a409d1384832c1933c980f7f9
Janika Kalliokoski <janika.kalliokoski@gmail.com>
2023-03-01 13:02:30 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -12,10 +12,6 @@ def pylint(ctx):
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
-@task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
-
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
